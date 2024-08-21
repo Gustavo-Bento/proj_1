38a8ede9204df5c5bcbe8347c4c3356778feea28
Gustavo Ferreira <gfmb94@gmail.com>
2024-08-21 20:52:51 -0300

    Aula 02

---
+++ aula02-1/aula2-3.py
@@ -0,0 +1,5 @@
+nome = 'Gustavo'
+idade = 16
+print(nome,'tem',idade,'anos')
+print(type(nome))
+print(type(idade))
---
+++ aula02-1/aula2-4.py
@@ -0,0 +1,5 @@
+idade = 19
+if(idade>=18):
+    print('Maior de idade')
+else:
+    print('Menor de idade')
